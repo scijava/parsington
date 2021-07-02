3675eb687a433ab762e310f56744a3cafa668c20
Curtis Rueden <ctrueden@wisc.edu>
2021-07-02 09:19:10 -0500

    Happy New Year 2021

--- src/main/java/org/scijava/parsington/eval/AbstractEvaluator.java
+++ src/main/java/org/scijava/parsington/eval/AbstractEvaluator.java
@@ -2,7 +2,7 @@
  * #%L
  * Parsington: the SciJava mathematical expression parser.
  * %%
- * Copyright (C) 2015 - 2019 Board of Regents of the University of
+ * Copyright (C) 2015 - 2021 Board of Regents of the University of
  * Wisconsin-Madison.
  * %%
  * Redistribution and use in source and binary forms, with or without
