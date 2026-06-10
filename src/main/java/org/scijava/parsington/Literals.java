5ae3ba2760d8f1d190f4c20c60aba75388adbe09
Jared Davis <sunray@davisprogramming.com>
2026-06-10 14:55:37 -0400

    Block Integer parsing of a string if it contains a decimal point.

--- src/main/java/org/scijava/parsington/Literals.java
+++ src/main/java/org/scijava/parsington/Literals.java
@@ -407,8 +407,9 @@ public final class Literals {
 		final boolean forceLong = "l".equalsIgnoreCase(force);
 		final boolean forceFloat = "f".equalsIgnoreCase(force);
 		final boolean forceDouble = "d".equalsIgnoreCase(force);
+		final int dot = number.indexOf(".");
 		Number result = null;
-		if (!forceFloat && !forceDouble) {
+		if ((dot == -1) && !forceFloat && !forceDouble) {
 			result = parseInteger(number, forceLong, 10);
 		}
 		if (result == null && !forceLong) {
